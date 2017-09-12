150de05c1a57b2b2803f276654d148eee7b8f696
Mladen Lotar <lotar.mladen@gmail.com>
2017-09-12 12:23:25 +0200

    Cleanup of application flow in unit tests

--- Application/test/Application/CrudApi/Controller/ResourceTest.php
+++ Application/test/Application/CrudApi/Controller/ResourceTest.php
@@ -2,14 +2,7 @@
 
 namespace ApplicationTest\Application\CrudApi\Controller;
 
-use Application\CrudApi\Module as CrudApiModule;
-use Framework\Base\Application\BaseApplication;
-use Framework\Base\Request\RequestInterface;
 use Framework\Base\Test\UnitTest;
-use Framework\Http\Request\Request;
-use Framework\RestApi\Module as RestApiModule;
-use Framework\RestApi\RestApi;
-use Framework\Terminal\Render\Memory as MemoryRenderer;
 
 /**
  * Class ResourceTest
@@ -17,79 +10,7 @@ use Framework\Terminal\Render\Memory as MemoryRenderer;
  */
 class ResourceTest extends UnitTest
 {
-    private $application = null;
-
-    public function __construct($name = null, array $data = [], $dataName = '')
-    {
-        parent::__construct($name, $data, $dataName);
-
-        $this->application = new RestApi([
-            RestApiModule::class,
-            CrudApiModule::class
-        ]);
-
-        // Remove render events from the application
-        $this->application->removeEventListeners(BaseApplication::EVENT_APPLICATION_RENDER_RESPONSE_PRE);
-        $this->application->removeEventListeners(BaseApplication::EVENT_APPLICATION_RENDER_RESPONSE_POST);
-    }
-
-    public function runApplication(RequestInterface $request)
-    {
-        $app = $this->application;
-
-        $app->setRequest($request);
-
-        $app->setRenderer(new MemoryRenderer());
-
-        try {
-            $app->run();
-        } catch (\Exception $e) {
-            $app->getExceptionHandler()
-                ->handle($e);
-        }
-
-        return $app;
-    }
-
-    /**
-     * @param string $method
-     * @param string $path
-     * @param array $parameters
-     * @param array $files
-     * @return \Framework\Base\Response\ResponseInterface
-     */
-    public function makeHttpRequest(
-        string $method,
-        string $path,
-        array $parameters = [],
-        array $files = []
-    ) {
-        // Normalize input
-        $method = strtoupper($method);
-
-        // Build basic http request
-        $request = new Request();
-        $request->setMethod($method)
-            ->setUri($path);
-
-        switch ($method) {
-            case 'DELETE':
-            case 'PATCH':
-            case 'PUT':
-            case 'POST':
-                $request->setPost($parameters);
-                $request->setFiles($files);
-                break;
-
-            default:
-                $request->setQuery($parameters);
-        }
-
-        return $this->runApplication($request)
-            ->getResponse();
-    }
-
-    public function testGetOneUser()
+    public function testGetAllUser()
     {
         $response = $this->makeHttpRequest('GET', '/users');
 
--- Modules/Base/src/Test/UnitTest.php
+++ Modules/Base/src/Test/UnitTest.php
@@ -6,6 +6,12 @@ use Framework\Base\Application\ApplicationInterface;
 use Framework\RestApi\Module;
 use Framework\RestApi\RestApi;
 use PHPUnit\Framework\TestCase;
+use Application\CrudApi\Module as CrudApiModule;
+use Framework\Base\Application\BaseApplication;
+use Framework\Base\Request\RequestInterface;
+use Framework\Http\Request\Request;
+use Framework\RestApi\Module as RestApiModule;
+use Framework\Terminal\Render\Memory as MemoryRenderer;
 
 /**
  * All tests should extend this class
@@ -19,6 +25,76 @@ class UnitTest extends TestCase
      * @var RestApi|null
      */
     private $application = null;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->application = new RestApi([
+            RestApiModule::class,
+            CrudApiModule::class
+        ]);
+
+        // Remove render events from the application
+        $this->application->removeEventListeners(BaseApplication::EVENT_APPLICATION_RENDER_RESPONSE_PRE);
+        $this->application->removeEventListeners(BaseApplication::EVENT_APPLICATION_RENDER_RESPONSE_POST);
+    }
+
+    public function runApplication(RequestInterface $request)
+    {
+        $app = $this->application;
+
+        $app->setRequest($request);
+
+        $app->setRenderer(new MemoryRenderer());
+
+        try {
+            $app->run();
+        } catch (\Exception $e) {
+            $app->getExceptionHandler()
+                ->handle($e);
+        }
+
+        return $app;
+    }
+
+    /**
+     * @param string $method
+     * @param string $path
+     * @param array $parameters
+     * @param array $files
+     * @return \Framework\Base\Response\ResponseInterface
+     */
+    public function makeHttpRequest(
+        string $method,
+        string $path,
+        array $parameters = [],
+        array $files = []
+    ) {
+        // Normalize input
+        $method = strtoupper($method);
+
+        // Build basic http request
+        $request = new Request();
+        $request->setMethod($method)
+            ->setUri($path);
+
+        switch ($method) {
+            case 'DELETE':
+            case 'PATCH':
+            case 'PUT':
+            case 'POST':
+                $request->setPost($parameters);
+                $request->setFiles($files);
+                break;
+
+            default:
+                $request->setQuery($parameters);
+        }
+
+        return $this->runApplication($request)
+            ->getResponse();
+    }
 
     /**
      * @return RestApi|null

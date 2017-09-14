11795008be5bf087bb9e8572eae4c62f98291c74
Mladen Lotar <lotar.mladen@gmail.com>
2017-09-14 16:06:25 +0200

    Move field modifiers to Base\Model namespace

--- Modules/Base/src/Model/Modifiers/FieldModifierInterface.php
+++ Modules/Base/src/Model/Modifiers/FieldModifierInterface.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Framework\Base\Repository\Modifiers;
+namespace Framework\Base\Model\Modifiers;
 
 /**
  * Interface FieldModifierInterface
- * @package Framework\Base\RepositoryManager\Modifiers
+ * @package Framework\Base\Model\Modifiers
  */
 interface FieldModifierInterface
 {

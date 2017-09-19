3bfb17bede48a628d0caa91959c0aa316446606e
Josip Brčina <info1x2@gmail.com>
2017-09-19 13:53:47 +0200

    Add terminal dispatcher, add terminal entry point - yoda

---
+++ Modules/Base/src/Terminal/Module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laptop02
+ * Date: 19.09.17.
+ * Time: 13:24
+ */
+
+namespace Framework\Base\Terminal;
+
+
+class Module
+{
+
+}
---
+++ Modules/Base/src/Terminal/Router/Dispatcher.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laptop02
+ * Date: 19.09.17.
+ * Time: 13:32
+ */
+
+namespace Framework\Base\Terminal\Dispatcher;
+
+
+class Dispatcher
+{
+
+}

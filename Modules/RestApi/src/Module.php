7a50fbcf92c8a6d229227559024d481087e358f7
Josip Brčina <info1x2@gmail.com>
2017-10-11 14:06:57 +0200

    Small change with defining routes due to last merge with master

--- Modules/RestApi/src/Module.php
+++ Modules/RestApi/src/Module.php
@@ -40,17 +40,17 @@ class Module extends BaseModule
                             '/login',
                             '\Framework\Base\Auth\Controller\AuthController::authenticate',
                         ],
+                        [
+                            'post',
+                            '/forgotPassword',
+                            '\Framework\Base\Auth\Controller\AuthController::forgotPassword',
+                        ],
+                        [
+                            'post',
+                            '/resetPassword',
+                            '\Framework\Base\Auth\Controller\AuthController::resetPassword',
+                        ],
                     ],
-                    [
-                        'post',
-                        '/forgotPassword',
-                        '\Framework\Base\Auth\Controller\AuthController::forgotPassword',
-                    ],
-                    [
-                        'post',
-                        '/resetPassword',
-                        '\Framework\Base\Auth\Controller\AuthController::resetPassword',
-                    ]
                 ]
             );
             $appConfig->setPathValue('routes.withoutPrefix', []);

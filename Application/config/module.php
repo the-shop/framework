90fa5b9b171b73dd942a7b679972d04d11791a1f
Josip Brčina <info1x2@gmail.com>
2017-10-17 09:41:38 +0200

    Add task model to models.json and add task adapters to module.php

--- Application/config/module.php
+++ Application/config/module.php
@@ -50,11 +50,15 @@ return [
         ],
         'sprints' => [
             MongoAdapter::class
+        ],
+        'tasks' => [
+            MongoAdapter::class
         ]
     ],
     'primaryModelAdapter' => [
         'users' => MongoAdapter::class,
         'projects' => MongoAdapter::class,
         'sprints' => MongoAdapter::class,
+        'tasks' => MongoAdapter::class,
     ],
 ];

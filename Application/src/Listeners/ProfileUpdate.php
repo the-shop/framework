a952ab7cc3e02d7ef669d7642bed84650f648de5
Josip Brčina <info1x2@gmail.com>
2017-10-26 15:00:36 +0200

    Write unit tests

--- Application/src/Listeners/ProfileUpdate.php
+++ Application/src/Listeners/ProfileUpdate.php
@@ -46,6 +46,15 @@ class ProfileUpdate implements ListenerInterface
                 $emailMessage
             );
 
+            $slack = $payload->getAttribute('slack');
+
+            if ($slack !== null && empty($slack) === false) {
+                //Send slack message with XP status changed
+                $recipient = '@' . $slack;
+                //TODO: implement after slack service is implemented
+                //Slack::sendMessage($recipient, $message, Slack::HIGH_PRIORITY);
+            }
+
             $subject = 'Xp status changed!';
             $html = /** @lang text */
                 "<html>
@@ -54,26 +63,19 @@ class ProfileUpdate implements ListenerInterface
                     </body>
                  </html>";
 
-
             $appConfig = $this->getApplication()->getConfiguration();
             $mailSender = $this->getApplication()->getService('emailService');
             /**
              * @var EmailService $mailSender
              */
-            $mailSender->sendEmail(
+            if ($mailResponse = $mailSender->sendEmail(
                 $appConfig->getPathValue('env.PRIVATE_MAIL_FROM'),
                 $subject,
                 $payload->getAttribute('email'),
                 $html
-            );
-
-            $slack = $payload->getAttribute('slack');
-
-            if ($slack !== null && empty($slack) === false) {
-                //Send slack message with XP status changed
-                $recipient = '@' . $slack;
-                //TODO: implement after slack service is implemented
-                //Slack::sendMessage($recipient, $message, Slack::HIGH_PRIORITY);
+            )
+            ) {
+                return $mailResponse;
             }
         }
 

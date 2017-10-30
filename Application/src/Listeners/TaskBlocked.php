75b9244782c11b6ca7a0a89f7471e0bb9a4c204b
ivankristic <thechosen.os@gmail.com>
2017-10-30 15:23:57 +0100

    TaskBlockedListener  - [x] new `TaskBlocked` listener class that sends slack message to the project owner  - [] register on bootstrap

---
+++ Application/src/Listeners/TaskBlocked.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Application\Listeners;
+
+use Application\Services\SlackService;
+use Framework\Base\Application\ApplicationAwareTrait;
+use Framework\Base\Event\ListenerInterface;
+use Framework\Base\Model\BrunoInterface;
+
+class TaskBlocked implements ListenerInterface
+{
+    use ApplicationAwareTrait;
+
+    /**
+     * @param BrunoInterface $payload
+     *
+     * @return mixed
+     */
+    public function handle($payload)
+    {
+        // Check if payload is BrunoInterface model and if collection is users
+        if (($payload instanceof BrunoInterface) === true|| $payload->getCollection() === 'tasks') {
+            $updatedFields = $payload->getDirtyAttributes();
+
+            if (empty($payload) === false) {
+                if (key_exists('blocked', $updatedFields) && $updatedFields['blocked'] === true) {
+                    $service = $this->getApplication()
+                                    ->getService(SlackService::class);
+
+                    if ($service->getApiClient() === null) {
+                        $service->setApiClient();
+                    }
+
+                    $repositoryManager = $this->getApplication()
+                                              ->getRepositoryManager();
+
+                    $project = $repositoryManager->getRepositoryFromResourceName('tasks')
+                                                 ->loadOne($payload->getAttribute('project_id'));
+
+                    $owner = $repositoryManager->getRepositoryFromResourceName('users')
+                                               ->loadOne($project->getAttribute('acceptedBy'));
+
+                    if ($owner instanceof BrunoInterface
+                        && ($recipient = $owner->getAttribute('slack')) !== null) {
+                        $webDomain = $this->getApplication()
+                                          ->getConfiguration()
+                                          ->getPathValue('env.WEB_DOMAIN');
+
+                        $message = 'Hey, task *'
+                                   . $payload->getAttribute('title')
+                                   . '* is currently blocked! '
+                                   . $webDomain
+                                   . 'projects/'
+                                   . $project->getId()
+                                   . '/sprints/'
+                                   . $payload->getAttribute('sprint_id')
+                                   . '/tasks/'
+                                   . $payload->getId();
+
+                        $service->setMessage($recipient, $message, false, SlackService::HIGH_PRIORITY);
+
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
